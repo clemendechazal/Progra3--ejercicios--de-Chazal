493fa5b717166ab0a4a83984929da6e14b8bfdad
clemendechazal <cdechazal@udesa.edu.ar>
2025-10-13 18:34:08 -0300

    practica3, me quede en el punto 2

---
+++ Clase9/practica3-continuacion/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ProductAll from './screens/ProductAll';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+       <ProductAll/>
+    </View>
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Clase9/practica3-continuacion/src/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function Card() {
+  return (
+    <View>
+        <text></text>
+    </View>
+  )
+}
+
+export default Card
